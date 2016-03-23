1db0b04b812cf122d118d4fbcfed780715ffd569
Marcello Monachesi <marcello.monachesi@fokus.fraunhofer.de>
2016-03-23 12:58:16 +0100

    Configuration for enabling the fm-system to get Openbaton event from Rabbit

---
+++ fault-correlator/src/main/java/org.openbaton.faultmanagement.fc/ConfigurationBeans.java
@@ -0,0 +1,112 @@
+package org.openbaton.faultmanagement.fc;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by mob on 23.03.16.
+ */
+@Configuration
+@ComponentScan("org.openbaton.faultmanagement.fc")
+public class ConfigurationBeans {
+    public static final String queueName_eventInstatiateFinish = "nfvo.fm.nsr.create";
+    public static final String queueName_eventResourcesReleaseFinish = "nfvo.fm.nsr.delete";
+    private Logger logger;
+
+    @PostConstruct
+    private void init(){
+        this.logger = LoggerFactory.getLogger(this.getClass());
+    }
+
+    @Bean
+    public Gson getMapper(){
+        return new GsonBuilder().serializeNulls().create();
+    }
+
+    @Bean
+    public ConnectionFactory getConnectionFactory(Environment env){
+        logger.debug("Created ConnectionFactory");
+        CachingConnectionFactory factory = new CachingConnectionFactory(env.getProperty("spring.rabbitmq.host"));
+        factory.setPassword(env.getProperty("spring.rabbitmq.password"));
+        factory.setUsername(env.getProperty("spring.rabbitmq.username"));
+        return factory;
+    }
+
+    @Bean
+    public TopicExchange getTopic(){
+        logger.debug("Created Topic Exchange");
+        return new TopicExchange("openbaton-exchange");
+    }
+
+    @Bean
+    public Queue getCreationQueue(){
+        logger.debug("Created Queue for NSR Create event");
+        return new Queue(queueName_eventInstatiateFinish,false,false,true);
+    }
+
+    @Bean
+    public Queue getDeletionQueue(){
+        logger.debug("Created Queue for NSR Delete Event");
+        return new Queue(queueName_eventResourcesReleaseFinish,false,false,true);
+    }
+
+    @Bean
+    public Binding setCreationBinding(@Qualifier("getCreationQueue") Queue queue, TopicExchange topicExchange){
+        logger.debug("Created Binding for NSR Creation event");
+        return BindingBuilder.bind(queue).to(topicExchange).with("ns-creation");
+    }
+
+    @Bean
+    public Binding setDeletionBinding(@Qualifier("getDeletionQueue") Queue queue, TopicExchange topicExchange){
+        logger.debug("Created Binding for NSR Deletion event");
+        return BindingBuilder.bind(queue).to(topicExchange).with("ns-deletion");
+    }
+
+    @Bean
+    public MessageListenerAdapter setCreationMessageListenerAdapter(OpenbatonEventReceiver receiver){
+        return new MessageListenerAdapter(receiver,"receiveNewNsr");
+    }
+
+    @Bean
+    public MessageListenerAdapter setDeletionMessageListenerAdapter(OpenbatonEventReceiver receiver){
+        return new MessageListenerAdapter(receiver,"deleteNsr");
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer setCreationMessageContainer(ConnectionFactory connectionFactory, @Qualifier("getCreationQueue") Queue queue, @Qualifier("setCreationMessageListenerAdapter") MessageListenerAdapter adapter){
+        logger.debug("Created MessageContainer for NSR Creation event");
+        SimpleMessageListenerContainer res = new SimpleMessageListenerContainer();
+        res.setConnectionFactory(connectionFactory);
+        res.setQueues(queue);
+        res.setMessageListener(adapter);
+        return res;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer setDeletionMessageContainer(ConnectionFactory connectionFactory, @Qualifier("getDeletionQueue") Queue queue, @Qualifier("setDeletionMessageListenerAdapter") MessageListenerAdapter messageListenerAdapter){
+        logger.debug("Created MessageContainer for NSR Deletion event");
+        SimpleMessageListenerContainer res = new SimpleMessageListenerContainer();
+        res.setConnectionFactory(connectionFactory);
+        res.setQueues(queue);
+        res.setMessageListener(messageListenerAdapter);
+        return res;
+    }
+
+}
---
+++ fault-correlator/src/main/java/org.openbaton.faultmanagement.fc/OpenbatonEventReceiver.java
@@ -0,0 +1,78 @@
+package org.openbaton.faultmanagement.fc;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import org.kie.api.runtime.KieSession;
+import org.openbaton.faultmanagement.fc.exceptions.NFVORequestorException;
+import org.openbaton.faultmanagement.fc.policymanagement.interfaces.PolicyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by mob on 23.03.16.
+ */
+@Service
+public class OpenbatonEventReceiver {
+    private static final Logger logger = LoggerFactory.getLogger(OpenbatonEventReceiver.class);
+
+    @Autowired private Gson mapper;
+    @Autowired private PolicyManager policyManager;
+    @Autowired private KieSession kieSession;
+
+
+    public void receiveNewNsr(String message) {
+        OpenbatonEvent openbatonEvent;
+        try {
+            openbatonEvent = getOpenbatonEvent(message);
+        } catch (NFVORequestorException e) {
+            logger.warn(e.getMessage());
+            return;
+        }
+
+        logger.debug("Received nfvo event with action: " + openbatonEvent.getAction());
+
+        boolean isNSRManaged = policyManager.isNSRManaged(openbatonEvent.getPayload().getId());
+        //Here we consider every instantiatie finish as recovery action finished
+        recoveryActionFinished();
+
+        if (!isNSRManaged)
+            policyManager.manageNSR(openbatonEvent.getPayload());
+    }
+
+    public void deleteNsr(String message){
+        OpenbatonEvent openbatonEvent;
+        try {
+            openbatonEvent = getOpenbatonEvent(message);
+        } catch (NFVORequestorException e) {
+            logger.warn(e.getMessage());
+            return;
+        }
+        logger.debug("Received nfvo event with action: " + openbatonEvent.getAction());
+        try {
+            boolean isNSRManaged = policyManager.isNSRManaged(openbatonEvent.getPayload().getId());
+            if (isNSRManaged)
+                policyManager.unManageNSR(openbatonEvent.getPayload());
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+        }
+    }
+
+    private OpenbatonEvent getOpenbatonEvent(String message) throws NFVORequestorException {
+        OpenbatonEvent openbatonEvent;
+
+        try {
+            openbatonEvent = mapper.fromJson(message, OpenbatonEvent.class);
+        } catch (JsonParseException e) {
+            throw new NFVORequestorException(e.getMessage(),e);
+        }
+        return openbatonEvent;
+    }
+
+    private void recoveryActionFinished() {
+        RecoveryAction recoveryAction = new RecoveryAction(RecoveryActionType.SWITCH_TO_STANDBY,"","");
+        recoveryAction.setStatus(RecoveryActionStatus.FINISHED);
+        kieSession.insert(recoveryAction);
+    }
+}
