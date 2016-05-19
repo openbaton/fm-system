69b7beef761e022b089e0f67dfb855a1a25337dc
Marcello Monachesi <marcello.monachesi@fokus.fraunhofer.de>
2016-05-19 11:37:33 +0200

    Updated licence and changed payload type
    Now the payload is a JsonObject so that the payload could be a VNFR or a NSR

--- fault-correlator/src/main/java/org.openbaton.faultmanagement.fc/OpenbatonEvent.java
+++ fault-correlator/src/main/java/org.openbaton.faultmanagement.fc/OpenbatonEvent.java
@@ -1,5 +1,22 @@
+/*
+* Copyright (c) 2015-2016 Fraunhofer FOKUS
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.openbaton.faultmanagement.fc;
 
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
 import org.openbaton.catalogue.mano.record.NetworkServiceRecord;
 import org.openbaton.catalogue.nfvo.Action;
 
@@ -10,7 +27,8 @@ import java.io.Serializable;
  */
 public class OpenbatonEvent implements Serializable{
     private Action action;
-    private NetworkServiceRecord payload;
+
+    private JsonObject payload;
 
     public OpenbatonEvent() {
     }
@@ -23,11 +41,11 @@ public class OpenbatonEvent implements Serializable{
         this.action = action;
     }
 
-    public NetworkServiceRecord getPayload() {
+    public JsonObject getPayload() {
         return payload;
     }
 
-    public void setPayload(NetworkServiceRecord payload) {
+    public void setPayload(JsonObject payload) {
         this.payload = payload;
     }
 }
