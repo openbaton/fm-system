cfa1ccd84f3376c1d3940189916a0e426eb4cba3
Marcello Monachesi <marcello.monachesi@fokus.fraunhofer.de>
2016-05-19 11:52:42 +0200

    Updated licence and added vnfr subscription

--- fault-correlator/src/main/java/org.openbaton.faultmanagement.fc/policymanagement/PolicyManagerImpl.java
+++ fault-correlator/src/main/java/org.openbaton.faultmanagement.fc/policymanagement/PolicyManagerImpl.java
@@ -1,3 +1,18 @@
+/*
+* Copyright (c) 2015-2016 Fraunhofer FOKUS
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.openbaton.faultmanagement.fc.policymanagement;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -9,6 +24,7 @@ import org.openbaton.catalogue.mano.record.NetworkServiceRecord;
 import org.openbaton.catalogue.mano.record.Status;
 import org.openbaton.catalogue.mano.record.VNFCInstance;
 import org.openbaton.catalogue.mano.record.VirtualNetworkFunctionRecord;
+import org.openbaton.catalogue.nfvo.Action;
 import org.openbaton.exceptions.MonitoringException;
 import org.openbaton.exceptions.NotFoundException;
 import org.openbaton.faultmanagement.fc.exceptions.FaultManagementPolicyException;
@@ -18,10 +34,12 @@ import org.openbaton.faultmanagement.fc.policymanagement.catalogue.NetworkServic
 import org.openbaton.faultmanagement.fc.policymanagement.catalogue.VNFCInstanceShort;
 import org.openbaton.faultmanagement.fc.policymanagement.catalogue.VirtualDeploymentUnitShort;
 import org.openbaton.faultmanagement.fc.policymanagement.catalogue.VirtualNetworkFunctionRecordShort;
+import org.openbaton.faultmanagement.fc.policymanagement.interfaces.EventSubscriptionManger;
 import org.openbaton.faultmanagement.fc.policymanagement.interfaces.MonitoringManager;
 import org.openbaton.faultmanagement.fc.policymanagement.interfaces.PolicyManager;
 import org.openbaton.faultmanagement.ha.HighAvailabilityManager;
 import org.openbaton.faultmanagement.ha.exceptions.HighAvailabilityException;
+import org.openbaton.sdk.api.exception.SDKException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,9 +71,12 @@ public class PolicyManagerImpl implements PolicyManager {
 
     @Autowired private MonitoringManager monitoringManager;
 
+    @Autowired private EventSubscriptionManger eventSubscriptionManger;
+
     @Autowired private HighAvailabilityManager highAvailabilityManager;
+
     @Value("${fms.redundancycheck:60}")
-    private String reduncancyCheck;
+    private String redundancyCheck;
 
     @PostConstruct
     public void init(){
@@ -64,29 +85,46 @@ public class PolicyManagerImpl implements PolicyManager {
     }
 
     @Override
-    public void manageNSR(NetworkServiceRecord nsr){
+    public void manageNSR(NetworkServiceRecord nsr) throws SDKException {
         if(!nsrNeedsMonitoring(nsr)){
-            log.info("The NSR"+ nsr.getName()+" needn't fault management monitoring");
+            log.info("The NSR"+ nsr.getName()+" doesn't need fault management monitoring");
             return;
         }
-        else {
-            log.debug("The NSR" + nsr.getName() + " need fault management monitoring");
-            NetworkServiceRecordShort nsrs = null;
-            try {
-                nsrs = getNSRShort(nsr);
-            } catch (FaultManagementPolicyException e) {
-                log.error("Getting the NSR short for the nsr: "+nsr.getName()+" "+e.getMessage(),e);
-            }
-            networkServiceRecordShortList.add(nsrs);
-
-            monitoringManager.startMonitorNS(nsr);
-
-            HighConfigurator highConfigurator = new HighConfigurator(nsr);
-            int interval = Integer.parseInt(reduncancyCheck);
-            futures.put(nsr.getId(),nsScheduler.scheduleAtFixedRate(highConfigurator,5,interval, TimeUnit.SECONDS));
+        List<VirtualNetworkFunctionRecord> vnfrRequiringFaultManagement = getVnfrRequiringFaultManagement(nsr);
+        log.debug("Those vnfr requires FM: "+vnfrRequiringFaultManagement);
+        for(VirtualNetworkFunctionRecord vnfr : vnfrRequiringFaultManagement){
+            eventSubscriptionManger.subscribe(vnfr, Action.HEAL);
+        }
+        log.debug("The NSR" + nsr.getName() + " needs fault management monitoring");
+        NetworkServiceRecordShort nsrs = null;
+        try {
+            nsrs = getNSRShort(nsr);
+        } catch (FaultManagementPolicyException e) {
+            log.error("Getting the NSR short for the nsr: "+nsr.getName()+" "+e.getMessage(),e);
         }
 
+        networkServiceRecordShortList.add(nsrs);
+
+        monitoringManager.startMonitorNS(nsr);
+
+        HighConfigurator highConfigurator = new HighConfigurator(nsr);
+        int interval = Integer.parseInt(redundancyCheck.trim());
+        log.debug("Starting redundancy thread every: "+interval+" seconds");
+        futures.put(nsr.getId(),nsScheduler.scheduleAtFixedRate(highConfigurator,5,interval, TimeUnit.SECONDS));
+
+
     }
+
+    private List<VirtualNetworkFunctionRecord> getVnfrRequiringFaultManagement(NetworkServiceRecord nsr) {
+        List<VirtualNetworkFunctionRecord> result = new ArrayList<>();
+        for (VirtualNetworkFunctionRecord vnfr : nsr.getVnfr()){
+            for(VirtualDeploymentUnit vdu : vnfr.getVdu())
+                if(vdu.getFault_management_policy()!= null && !vdu.getFault_management_policy().isEmpty())
+                    result.add(vnfr);
+        }
+        return result;
+    }
+
     private boolean nsrNeedsMonitoring(NetworkServiceRecord nsr) {
         if(nsr.getFaultManagementPolicy() != null && !nsr.getFaultManagementPolicy().isEmpty())
             return true;
@@ -236,19 +274,20 @@ public class PolicyManagerImpl implements PolicyManager {
                 log.error(e.getMessage(),e);
             }
             if(nsr.getStatus().ordinal() != Status.ACTIVE.ordinal()) {
-                //log.debug("the nsr to check redundancy is not in ACTIVE state");
+                log.debug("Redundancy thread: the nsr to check redundancy is not in ACTIVE state");
                 return;
             }
             for (VirtualNetworkFunctionRecord vnfr : nsr.getVnfr()) {
                 try {
-                    // If there are instance to be removed
+                    // If there are instances to be removed
                     String failedVNFCInstance = highAvailabilityManager.cleanFailedInstances(vnfr);
                     if (failedVNFCInstance!=null){
                         log.info("Cleaning procedure start for: "+failedVNFCInstance);
                         monitoringManager.removeMonitoredVnfcInstance(failedVNFCInstance);
                     }
-                    else
+                    else{
                         highAvailabilityManager.configureRedundancy(vnfr);
+                    }
                 } catch (HighAvailabilityException e) {
                     log.error("Configuration of the redundancy for the vnfr: "+vnfr.getName()+" "+e.getMessage(),e);
                 }catch (UnirestException e) {
