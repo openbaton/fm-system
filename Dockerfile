b55e69bf7b7f19a828a96c0f12c20f0740b23bc5
flaviomu <flaviomurgiasrvcs@gmail.com>
2017-11-27 12:57:42 +0100

    Fix: update dockerfile for service support

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ RUN ./gradlew build -x test
 FROM openjdk:8-jre-alpine
 COPY --from=builder /project/build/libs/*.jar /fms.jar
 RUN mkdir -p /var/log/openbaton
+RUN apk add -u --no-cache python3 python3-dev curl && pip3 install openbaton-cli==5.0.0rc1
+COPY --from=builder /project/gradle/gradle/scripts/docker/fms.sh /fms.sh
 COPY --from=builder /project/etc/fms.properties /etc/openbaton/fms.properties
-ENTRYPOINT ["java", "-jar", "/fms.jar", "--spring.config.location=file:/etc/openbaton/fms.properties"]
+ENTRYPOINT ["/fms.sh"]
 EXPOSE 9000
---
+++ gradle/gradle/scripts/docker/fms.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+FMS_SERVICE_KEY=$(grep fms.service.key /etc/openbaton/openbaton-fms.properties|cut -d'=' -f 2)
+
+if [ -z "$FMS_SERVICE_KEY" ];then
+    until curl -sSf http://nfvo:8080;do sleep 10;done
+
+    USER=admin
+    PASS=openbaton
+    NFVO_IP=nfvo
+    NFVO_PORT=8080
+    PID=$(openbaton -pid none -u "$USER" -p "$PASS" -ip "$NFVO_IP" --nfvo-port "$NFVO_PORT" project list|grep default|awk '{print $2}')
+    SERVICE_KEY=$(openbaton -pid "$PID" -u "$USER" -p "$PASS" -ip "$NFVO_IP" --nfvo-port "$NFVO_PORT" service create '{"name":"fms", "roles":["*"]}')
+
+    export FMS_SERVICE_KEY="$SERVICE_KEY"
+    sed -i "s/fms.service.key =/fms.service.key=$SERVICE_KEY/g" /etc/openbaton/openbaton-fms.properties
+fi
+
+exec java -jar /fms.jar --spring.config.location=file:/etc/openbaton/openbaton-fms.properties
